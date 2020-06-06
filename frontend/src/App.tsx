5a17ef860b0093a3f2bebb40eed894bc20278844
César Augusto Costa <cezarcozta@gmail.com>
2020-06-06 02:32:08 -0300

    final commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+import Routes from './routes';
+
+const App: React.FC = () => {
+  return (
+    <div>   
+      <Routes />
+    </div>
+  );
+};
+
+export default App;
